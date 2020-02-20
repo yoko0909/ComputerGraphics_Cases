9b0775c36af8f1639a55917339fb80e61b3277e0
Guoyoko <guoyoko@outlook.com>
2020-02-20 23:48:30 +0800

    综合案例11：鼠标交互绘制直线

---
+++ 综合案例11：鼠标交互绘制直线/TestView.cpp
@@ -0,0 +1,144 @@
+
+// TestView.cpp : implementation of the CTestView class
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "Test.h"
+#endif
+#include "TestDoc.h"
+#include "TestView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CTestView
+
+IMPLEMENT_DYNCREATE(CTestView, CView)
+
+BEGIN_MESSAGE_MAP(CTestView, CView)
+	// Standard printing commands
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CTestView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+// CTestView construction/destruction
+
+CTestView::CTestView()
+{
+	// TODO: add construction code here
+}
+
+CTestView::~CTestView()
+{
+}
+
+BOOL CTestView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: Modify the Window class or styles here by modifying
+	//  the CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CTestView drawing
+
+void CTestView::OnDraw(CDC* pDC)
+{
+	CTestDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: add draw code for native data here	
+}
+
+
+// CTestView printing
+
+
+void CTestView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CTestView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// default preparation
+	return DoPreparePrinting(pInfo);
+}
+
+void CTestView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add extra initialization before printing
+}
+
+void CTestView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add cleanup after printing
+}
+
+void CTestView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CTestView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CTestView diagnostics
+
+#ifdef _DEBUG
+void CTestView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CTestView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CTestDoc* CTestView::GetDocument() const // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CTestDoc)));
+	return (CTestDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+
+void CTestView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	P0 = point;
+	CView::OnLButtonDown(nFlags, point);
+}
+
+
+void CTestView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	P1 = point;
+	CDC* pDC=GetDC();
+	pDC->MoveTo(P0);
+	pDC->LineTo(P1);
+	ReleaseDC(pDC);
+	CView::OnLButtonUp(nFlags, point);
+}
---
+++ 综合案例11：鼠标交互绘制直线/TestView.h
@@ -0,0 +1,54 @@
+
+// TestView.h : interface of the CTestView class
+//
+
+#pragma once
+
+class CTestView : public CView
+{
+protected: // create from serialization only
+	CTestView();
+	DECLARE_DYNCREATE(CTestView)
+
+// Attributes
+public:
+	CTestDoc* GetDocument() const;
+
+// Operations
+public:
+	void DrawObject(CDC* pDC);//绘制对象
+// Overrides
+public:
+	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// Implementation
+public:
+	virtual ~CTestView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+	CPoint P0,P1;// 直线的起点与终点坐标
+// Generated message map functions
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // debug version in TestView.cpp
+inline CTestDoc* CTestView::GetDocument() const
+   { return reinterpret_cast<CTestDoc*>(m_pDocument); }
+#endif
+
